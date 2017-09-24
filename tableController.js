1bcf6b7be97719e99449a8e24709495c7fb0626e
jannchie <jannchie@gmail.com>
2017-09-24 22:50:53 +0800

    Lab1-1

--- tableController.js
+++ tableController.js
@@ -1,11 +1,22 @@
-﻿
+﻿v
+
+
+
+
 function addRow() {
     var table = document.getElementById("table");
     var tr = document.createElement("tr");
-    var array = [1, 3, 4, 2, 5];
+    var name = document.getElementById("nameText");
+    var age = document.getElementById("ageText");
+    var sex = document.getElementById("sexText");
+    var tel = document.getElementById("telText");
+    var adr = document.getElementById("addressText");
+
+
+    var array = [name.value, age.value, sex.value, tel.value, adr.value];
     for (i = 0; i < 5; i++) {
         var td = document.createElement("td");
-        var txt = document.createTextNode(array[i]);
+        var txt = document. createTextNode(array[i]);
         td.appendChild(txt);
         tr.appendChild(td);
         table.appendChild(tr);
